295461944e51e3624658672c565a68af5fc40630
theOpenBit <theopenbit@users.noreply.github.com>
2017-03-27 22:05:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir build
 cd build
 git clone https://github.com/docker-library/mariadb.git
-cd mariadb
+cd mariadb/10.0
 sed -i '1s/.*/FROM resin\/rpi-raspbian:jessie/' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
