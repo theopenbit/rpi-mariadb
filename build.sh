d2185db4951e5f54d230ef256cd63d550a3650fe
theOpenbit <tob@schoenesnetz.de>
2017-03-28 21:39:45 +0200

    wip

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ sed -i '59 i\RUN rm \/etc\/apt\/preferences.d\/percona' Dockerfile
 sed -i '61 i\RUN rm \/etc\/apt\/sources.list.d\/percona.list' Dockerfile
 sed -i '71 i\RUN rm \/etc\/apt\/preferences.d\/mariadb' Dockerfile
 sed -i '73 i\RUN rm \/etc\/apt\/sources.list.d\/mariadb.list' Dockerfile
+sed -i 's/mariadb-server=$MARIADB_VERSION /mariadb-server/g' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
 docker build -t theopenbit/rpi-mariadb:$DOCKER_TAG .
