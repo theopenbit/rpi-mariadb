8e0ac691f693fc0b7845a958d5b83737421c34d0
theOpenbit <tob@schoenesnetz.de>
2017-03-28 21:26:49 +0200

    wip

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ cd build
 git clone https://github.com/docker-library/mariadb.git
 cd mariadb/10.0
 sed -i '2s/.*/FROM resin\/rpi-raspbian:jessie/' Dockerfile
+sed -i '59 i\RUN rm \/etc\/apt\/preferences.d\/percona' Dockerfile
+sed -i '61 i\RUN rm \/etc\/apt\/sources.list.d\/percona.list' Dockerfile
+sed -i '71 i\RUN rm \/etc\/apt\/preferences.d\/mariadb' Dockerfile
+sed -i '73 i\RUN rm \/etc\/apt\/sources.list.d\/mariadb.list' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
 docker build -t theopenbit/rpi-mariadb:$DOCKER_TAG .
